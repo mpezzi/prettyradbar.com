6dad4c6c0e4c4359831b1e06e40d64deb1096140
Mike Pezzi <funkym@gmail.com>
2020-10-06 19:56:23 -0400

    feat(analytics): added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-179947051-1",
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
